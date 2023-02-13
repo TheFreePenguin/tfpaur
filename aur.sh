cbd2676597367671c9dd2699ed50b72ede097639
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2023-02-13 09:29:22 -0800

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -1,10 +1,10 @@
-if ${1} = ""; then
+if $1 = ""; then
   echo "Please enter a valid package name."
 else
 set -e
 mkdir /tmp/tfpaur
 cd /tmp/tfpaur
-git clone "https://aur.archlinux.org/${1}.git"
+git clone "https://aur.archlinux.org/$1.git"
 cd ${1}
 makepkg -si
 fi
