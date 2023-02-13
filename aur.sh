ed83d9e1d1e96c146b5bbdc0d7710d2c9e36eabe
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2023-02-13 05:35:15 -0800

    Create aur.sh

---
+++ aur.sh
@@ -0,0 +1,9 @@
+if ${1} = ""; then
+  echo "Please enter a valid package name."
+else
+set -e
+mkdir /tmp/tfpaur
+cd /tmp/tfpaur
+git clone "https://aur.archlinux.org/"${1}".git
+cd ${1}
+makepkg -si
