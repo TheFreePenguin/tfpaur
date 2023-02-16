b8661859b8f8b831c9533c09d0166ccb16a4e8d8
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2023-02-16 09:36:17 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ echo "Copying files [33]"
 cp aur.sh /usr/local/bin/tfpaur
 chmod +x /usr/local/bin/tfpaur
 echo "Creating uninstall binary [67]" 
-echo "rm /usr/local/bin/tfpaur" > /usr/local/bin/tfpaur-uninstall
+echo "rm /usr/local/bin/tfpaur; rm /usr/local/bin/tfpaur-uninstall" > /usr/local/bin/tfpaur-uninstall
 chmod +x /usr/local/bin/tfpaur-uninstall
-echo "Install complete! Run \"tfpaur\" to run the program. Run \"tfpaur-uninstall\" to uninstall the program."
+echo "Install complete! Run \"tfpaur\" to run the program. Run \"tfpaur-uninstall\" as root to uninstall the program."
