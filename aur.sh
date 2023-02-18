a4b675e4c7855ee6e63deca220b7b7ef309d29ad
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2023-02-18 14:43:14 -0800

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -1,6 +1,7 @@
 echo "Enter the packages you wish to install, one at a time. When you wish to continue, type EOF." 
 rm -rf /tmp/tfpaur
 mkdir -p /tmp/tfpaur
+cd /tmp/tfpaur
 read
 if [ $REPLY = "EOF" ]; then
 for i in `cat /tmp/tfpaur/packages.txt`
